0014ad3e789eca8341e005503703699a7bd60811
Charan Sai <csai4490@gmail.com>
2025-02-04 17:15:39 +0530

    All Errors cleared

--- SmartKnowledgeHub/src/App.jsx
+++ SmartKnowledgeHub/src/App.jsx
@@ -4,13 +4,11 @@ import Home from './Components/Home';
 import AboutUs from './Components/AboutUs';
 import AuthContainer from './Components/AuthContainer';
 import LandingPage from './Pages/LandingPage';
-import Login from './styles/login';
 function App() {
   
   return (
     <>
       <LandingPage/>
-      <Login />
     </>
   );
 }
