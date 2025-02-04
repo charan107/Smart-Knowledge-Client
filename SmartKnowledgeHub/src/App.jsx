c60e4982a7551d84974a6092b805e1c9794bdecd
Charan Sai <csai4490@gmail.com>
2025-02-04 20:00:26 +0530

    Removed Unneccessary Data

--- SmartKnowledgeHub/src/App.jsx
+++ SmartKnowledgeHub/src/App.jsx
@@ -1,8 +1,3 @@
-import { useState, useEffect } from 'react';
-import Navbar from './Components/Navbar';
-import Home from './Components/Home';
-import AboutUs from './Components/AboutUs';
-import AuthContainer from './Components/LearnWithUs';
 import LandingPage from './Pages/LandingPage';
 function App() {
   
